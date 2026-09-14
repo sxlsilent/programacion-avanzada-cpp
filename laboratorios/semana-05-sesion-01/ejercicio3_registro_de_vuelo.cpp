9a81824b863d20c614dd871efa854393edd9caa7
sxlsilent <maldonadobrandon236@gmail.com>
2026-09-14 13:10:09 +0000

    realizado ejercicio 3 semana 5 sesion 1

--- laboratorios/semana-05-sesion-01/ejercicio3_registro_de_vuelo.cpp
+++ laboratorios/semana-05-sesion-01/ejercicio3_registro_de_vuelo.cpp
@@ -1,16 +1,31 @@
 #include <iostream>
 
-// TODO: define aqui la clase RegistroDeVuelo, con:
-// - un puntero privado double* alturas y un int capacidad.
-// - un constructor RegistroDeVuelo(int nuevaCapacidad) que reserve
-//   memoria dinamica con new[] para "capacidad" valores double, e
-//   imprima "Registro de vuelo creado para <capacidad> lecturas".
-// - void guardarAltura(int indice, double valor).
-// - double getAltura(int indice).
-// - un destructor ~RegistroDeVuelo() que libere la memoria con delete[]
-//   e imprima "Registro de vuelo destruido, memoria liberada", para atar
-//   la vida de esa memoria a la vida del objeto (RAII).
 class RegistroDeVuelo {
+    private:
+        double* alturas;
+        int capacidad;
+
+    public:
+        RegistroDeVuelo(int nuevaCapacidad) {
+            capacidad = nuevaCapacidad;
+            alturas = new double[capacidad];
+
+            std::cout << "Registro de vuelo creado para " << capacidad << " lecturas" << std::endl;
+    }
+
+    void guardarAltura(int indice, double valor) {
+        alturas[indice] = valor;
+    }
+
+    double getAltura(int indice) {
+        return alturas[indice];
+    }
+
+    ~RegistroDeVuelo() {
+        delete[] alturas;
+
+        std::cout << "Registro de vuelo destruido, memoria liberada" << std::endl;
+    }
 };
 
 bool procesarVuelo(int capacidad, double alturaMinima) {
